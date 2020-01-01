cccd1569f63612475a631e06d24db0ff941dde02
ragith <ragith.coolkid@gmail.com>
2019-12-31 20:37:42 -0500

    Clean up

--- src/Exercise.js
+++ src/Exercise.js
@@ -2,23 +2,14 @@ import React, {useState, useEffect} from 'react';
 
 const Exercise = (props) => {
 	const sets =[]
-	const [remainingSets, setRemainingSets] = useState(props.exercise.Sets-1);
-	const completedSet = (e) => {
-		e.target.style.backgroundColor = '#CD5C5C';
-		setRemainingSets(remainingSets-1);
-		if(remainingSets == 0) {
-			props.completeExercise(props.index);
-		}
-	}
-	let styling = {};
-	if(props.exercise.Completed == 'true')
-		styling = {display: 'none'}
+
+	useEffect(() => console.log('test'));
 
 	for (var i = 0; i < props.exercise.Sets; i++) {
 		sets.push(
 			<li 
 			className="exercise_reps" 
-			onClick={completedSet} 
+			onClick={(e) => props.completedSet(e, props.index)} 
 			key={i}>
 			<span> {props.exercise.Reps} </span>
 			</li>
@@ -26,7 +17,7 @@ const Exercise = (props) => {
 	}
 
 	let exerciseSets;
-	if (props.exercise.Completed != 'true') {
+	if (props.exercise.remainingSets != 0) {
 		exerciseSets =	<ul key={props.exercise.id} className="exercise_sets"> {sets} </ul>;
 	}
 	else {
